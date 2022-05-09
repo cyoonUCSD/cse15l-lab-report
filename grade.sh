44e86287585a8df451eeec9525d0d4bd0b3748fe
cyoonUCSD <103143169+cyoonUCSD@users.noreply.github.com>
2022-05-08 23:50:05 -0700

    Create grade.sh

---
+++ grade.sh
@@ -0,0 +1,7 @@
+
+git clone $1
+cp GradingTests.java markdown-parser/
+cd markdown-parser
+javac -cp lib/*:. GradingTests.java MarkdownParse.java
+java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
+git push $1
